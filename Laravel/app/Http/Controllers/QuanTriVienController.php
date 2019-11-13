4108e8a749a8eee4ec43866ef0757093fcd14168
izaass <mr.izaass@gmail.com>
2019-11-14 00:30:48 +0800

    commit by phi hùng (update trang login.blade.php và css)

--- Laravel/app/Http/Controllers/QuanTriVienController.php
+++ Laravel/app/Http/Controllers/QuanTriVienController.php
@@ -20,7 +20,7 @@ class QuanTriVienController extends Controller
     	{
     		 return redirect()->route('trang-chu');
     	}
-		return "Đăng nhập thất bại";
+		return redirect('dang-nhap')->with('thongbao',"Sai tài khoản hoặc mật khẩu");
 
     	/*kiem tra tk va mat khau
     	$check = QuanTriVien::where('ten_dang_nhap',$ten_dang_nhap)->first();
@@ -36,7 +36,7 @@ class QuanTriVienController extends Controller
     public function layThongTin()
     {
     	$get = Auth::user();
-        return $get->ten_dang_nhap;
+        return $get;
     }
     public function dangXuat()
     {
